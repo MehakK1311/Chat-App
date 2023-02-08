ca286a3a27a8b17a1dabf8500c6b88d8ce377184
MehakK1311 <mehakhanna1311@gmail.com>
2023-02-08 23:45:23 +0530

    user Authentication

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,22 +1,43 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs')
 
-const userSchema = mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    email:{
-        type:String,
-        required:true
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    pic:{
-        type:String,
-        required:true,
-        default:"./assets/image/user"
+    password: {
+      type: "String",
+      required: true,
     },
-},{
-    timestamp: true
-});
+    pic: {
+      type: String,
+      default: "./assets/image/user",
+    },
+  },
+  {
+    timestamp: true,
+  }
+);
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+}
+
+userSchema.pre("save", async function (next){
+  if(!this.isModified){
+    next()
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+})
 
 const User = mongoose.model("User", userSchema);
 
